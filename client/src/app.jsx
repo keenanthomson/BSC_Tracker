8072d0feee0d491a95b4706a41d2d727c750c988
Keenan Thomson <keenanthomson@gmail.com>
2020-10-06 01:47:43 -0400

    react + webpack setup

---
+++ client/src/app.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export default function App(props) {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <button onClick={() => setCounter(counter + 1)}>You've clicked {counter} times!</button>
+  )
+}
