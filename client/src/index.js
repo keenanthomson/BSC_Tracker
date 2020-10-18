d5cf3098ad18e8a1643d9e7c2be8d23cc7ab6db6
Keenan Thomson <keenanthomson@gmail.com>
2020-10-18 00:06:42 -0400

    rhl work

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-hot-loader/patch';
 import App from './app.jsx';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./app.jsx', () => render(App))
+};
+
+// render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,24 @@ const SRC_DIR = path.join(__dirname, 'client/src');
 const DIST_DIR = path.join(__dirname, 'client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: [`${SRC_DIR}/index.js`,
+    'react-hot-loader/patch', // RHL patch
+    './scripts/index'], // Your appʼs entry point
   output: {
     path: DIST_DIR,
     filename: 'bundle.js'
   },
   module: {
+    // loaders: [{
+    //   test: /\.js$/,
+    //   loaders: ['react-hot-loader/webpack'],
+    // }],
     rules: [
       {
         test: /\.jsx|\.js/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: ['react-hot-loader/webpack','babel-loader'],
           options: {
             presets: ['@babel/react']
           }
